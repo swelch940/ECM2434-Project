5bb6303ec8a635b22be823113c57b6d95dfffb87
lhidalgogt2003 <95689705+lhidalgogt2003@users.noreply.github.com>
2023-02-27 15:29:47 +0000

    login/registration form

--- ECM2434-Project/ECM2434_Project/mysite/mysite/settings.py
+++ ECM2434-Project/ECM2434_Project/mysite/mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'home.apps.HomeConfig',
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK="bootstrap2"
+
