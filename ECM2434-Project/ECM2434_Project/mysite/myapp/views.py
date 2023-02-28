927b43bb600b68e428bc6c6416e6f642624a1a2c
lhidalgogt2003 <95689705+lhidalgogt2003@users.noreply.github.com>
2023-02-28 16:23:02 +0000

    login/registration

--- ECM2434-Project/ECM2434_Project/mysite/myapp/views.py
+++ ECM2434-Project/ECM2434_Project/mysite/myapp/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from mysite.forms import RegisterForm
 
 
 # Create your views here.
-def register(response):
-    if response.method == "POST":
-        form = RegisterForm(response.POST)
+
+
+def register(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
-        return redirect("/")
-    else:
-        form = RegisterForm()
-    return render(response, "registration/register.html", {"form":form})
+            return redirect("/")
 
+    context = {
+        'form':form,
+    }
+    return render(request, 'register/register.html',context)
