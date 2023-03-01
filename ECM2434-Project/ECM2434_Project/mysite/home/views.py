8cd39ede8d73b9f146cfb49d07821e9a2c58fb7e
lhidalgogt2003 <95689705+lhidalgogt2003@users.noreply.github.com>
2023-03-01 15:23:31 +0000

    authentication

--- ECM2434-Project/ECM2434_Project/mysite/home/views.py
+++ ECM2434-Project/ECM2434_Project/mysite/home/views.py
@@ -34,7 +34,7 @@ def register(request):
         form = RegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect("/")
+            return redirect("/tree")
 
     context = {
         'form':form,
--- ECM2434-Project/ECM2434_Project/mysite/mysite/settings.py
+++ ECM2434-Project/ECM2434_Project/mysite/mysite/settings.py
@@ -35,8 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap5',
     'home.apps.HomeConfig',
 ]
 
@@ -130,8 +128,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = "/tree"
