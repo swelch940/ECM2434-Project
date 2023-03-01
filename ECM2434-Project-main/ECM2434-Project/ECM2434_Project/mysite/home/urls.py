f137065e0dd27602fb4b0a8bad02d556a03aa87b
Cmwilton <99840882+Cmwilton@users.noreply.github.com>
2023-03-01 15:39:38 +0000

    Updated login + register pages
    Made it compatible with Luis's code

---
+++ ECM2434-Project-main/ECM2434-Project/ECM2434_Project/mysite/home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('map/', views.map, name='map'),
+    path('leaderboard/', views.leaderboard, name="leaderboard"),
+    path('admin/', admin.site.urls),
+    path('register/', views.register, name="register"),
+    path('tree', views.tree, name="tree"),
+    path('', include("django.contrib.auth.urls")),
+]
