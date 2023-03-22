aabfcf55f9333d9b785ff78fc92d0212327ac0ca
SporeCloud <44228349+SporeCloud@users.noreply.github.com>
2023-03-22 22:49:05 +0000

    Add files via upload
    added actual tree testing

--- ECM2434-Project/ECM2434_Project/mysite/home/tests.py
+++ ECM2434-Project/ECM2434_Project/mysite/home/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from home.models import Tree
+
 
 # Create your tests here.
 
@@ -29,3 +31,20 @@ class URLTests(TestCase):
     def test_registerpage(self):
         response = self.client.get('/register/')
         self.assertEqual(response.status_code, 200)
+
+class TreeTestCase(TestCase):
+    def setUp(self):
+        Tree.objects.create(username="harry")
+    
+    def assertValues(self):
+        x = Tree.objects.get(username="harry")
+        self.assertEqual(x.username,"harry")
+        self.assertEqual(x.oxygen,0)
+        self.assertEqual(x.level,1)
+        self.assertEqual(x.plastic_saved,0)
+        self.assertEqual(x.water,20)
+        self.assertEqual(x.in_greenhouse,False)
+        self.assertEqual(x.isAlive,True)
+        self.assertEqual(x.bottle_plastic,500)
+    
+    
