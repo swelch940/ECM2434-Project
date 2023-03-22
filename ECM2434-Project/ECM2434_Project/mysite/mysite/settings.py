5ab464cf755bb60ab6de68f170ee0a22ad9fd9fc
swelch940 <95835442+swelch940@users.noreply.github.com>
2023-03-22 10:38:40 +0000

    Update settings.py

--- ECM2434-Project/ECM2434_Project/mysite/mysite/settings.py
+++ ECM2434-Project/ECM2434_Project/mysite/mysite/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "/static"),
 ]
 
 # Default primary key field type
