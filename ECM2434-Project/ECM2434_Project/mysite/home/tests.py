3a75fc4d05234afb0c2a8227da55ca71f584c7f8
SporeCloud <44228349+SporeCloud@users.noreply.github.com>
2023-03-22 22:07:47 +0000

    Update tests.py
    deletions

--- ECM2434-Project/ECM2434_Project/mysite/home/tests.py
+++ ECM2434-Project/ECM2434_Project/mysite/home/tests.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-import asyncio
-from . import bark_buddy
-
 
 # Create your tests here.
 
@@ -32,40 +29,3 @@ class URLTests(TestCase):
     def test_registerpage(self):
         response = self.client.get('/register/')
         self.assertEqual(response.status_code, 200)
-
-
-#Currently under construction. 
-class BarkBuddyTest(TestCase):
-    
-    
-    testbuddy = bark_buddy.TestBuddy(12345, "test")
-    
-    #Test to see if the murder tree functions works.
-    def test_assert_murder(self):
-        self.testbuddy.murder_tree()
-        try:
-            assert self.testbuddy.alive == False
-        except(AssertionError):
-            return False
-        else: return True
-    
-    #Test to see if the water function of the tree is working.
-    def test_assert_water_tree(self):
-         try:
-             assert self.testbuddy.water + 5 == self.testbuddy.water_tree()
-         except(AssertionError):
-            return False
-         else: return True
-    
-    #Asserts that the tree can level up
-    def test_level_up(self):
-        try:
-            assert self.testbuddy.level_handling() == self.testbuddy.level + 1
-        except(AssertionError):
-            return False
-        else: return True
-
-
-#More extensive testing needed for the bark_buddy class as we integrate.
-    
-    
